faa0713d48ecc841884afff6dec718ca1ff916ca
“Brenno <dev.brennoeudes@gmail.com>
2024-05-02 15:21:08 -0300

    details.jsx

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Details } from "./pages/Details.jsx";
 
 // O ReactDom vai renderizar a div "root"!
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Details />
+  </React.StrictMode>
+);
