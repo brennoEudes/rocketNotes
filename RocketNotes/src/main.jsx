e655732b7e744dd24583ac43a303d2d7daff4231
“Brenno <dev.brennoeudes@gmail.com>
2024-05-02 14:51:41 -0300

    app, index.css deleted

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 // O ReactDom vai renderizar a div "root"!
 ReactDOM.createRoot(document.getElementById('root')).render(
