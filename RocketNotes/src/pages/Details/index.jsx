5cbbf8c0213e3ef05fd28d7879d75c5a3aa0bbb0
“Brenno <dev.brennoeudes@gmail.com>
2024-05-06 21:27:34 -0300

    main, Container; page details concluded

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./style";
+import { Container, Links, Content } from "./style";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
@@ -6,29 +6,43 @@ import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
 
-
 export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir nota"/>
-      <Section title="Links Úteis">
-        {/* O children não é passado como prop. comum, mas captura tudo q está dentro (abaixo): */}
-        <Links>
-          <li>
-            <a href="#">https://www.rockeseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rockeseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="Express"/>
-        <Tag title="NodeJS"/>
-      </Section>
-      {/* <Button title="Login" loading /> loading é prop. boleana e ñ é necessario dizer true/false */}
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis
+            neque, efficitur non odio sed, congue blandit odio. Sed finibus
+            risus id massa faucibus pharetra. Cras hendrerit tristique est ac
+            aliquam. Maecenas imperdiet enim neque, a tempor elit consequat sit
+            amet. Morbi in aliquam ipsum, ac feugiat enim. Praesent sit amet ex
+            eget sapien pretium semper ac vitae mi. Nam tortor orci, commodo eu
+            semper eu, ultricies eu sapien. Duis ultricies a ligula at feugiat.
+            In est orci, luctus vitae semper quis, congue eget ex.
+          </p>
+          <Section title="Links Úteis">
+            {/* O children não é passado como prop. comum, mas captura tudo q está dentro (abaixo): */}
+            <Links>
+              <li>
+                <a href="#">https://www.rockeseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rockeseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="NodeJS" />
+          </Section>
+          {/* <Button title="Login" loading /> loading é prop. boleana e ñ é necessario dizer true/false */}
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
--- RocketNotes/src/pages/Details/style.js
+++ RocketNotes/src/pages/Details/style.js
@@ -8,18 +8,51 @@ export const Container = styled.div`
   display: grid;
   grid-template-rows: 105px auto;
   grid-template-areas:
-  "header"
-  "content";
+    "header"
+    "content";
+
+  > main {
+    grid-area: content; /* Usando o main p/ aplicar região do grid. */
+    overflow-y: scroll; /* Barra de rolagem vertical */
+    padding: 64px 0;
+  }
 `;
 
 export const Links = styled.ul`
-list-style: none;
+  list-style: none;
 
-> li {
-  margin-top: 12px;
+  > li {
+    margin-top: 12px;
 
-  a {
-    color: ${({ theme }) => theme.COLORS.WHITE};
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
   }
-}
+`;
+
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+    text-align: justify;
+  }
+
+
+
 `;
