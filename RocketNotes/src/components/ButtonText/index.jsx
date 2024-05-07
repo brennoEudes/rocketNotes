e735b4fba07b794e446ebd396eaa0d154c730dc6
“Brenno <dev.brennoeudes@gmail.com>
2024-05-07 17:40:56 -0300

    Styled Component ButtonText correction

--- RocketNotes/src/components/ButtonText/index.jsx
+++ RocketNotes/src/components/ButtonText/index.jsx
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 // caso a prop "isActive" ñ seja definida, o valor padrão é falso.
 export function ButtonText({ title, isActive = false, ...rest }) {
   return (
-    <Container type="button" isActive={isActive} {...rest}>
+    <Container type="button" $isactive={isActive} {...rest}>
       {title}
     </Container>
   );
--- RocketNotes/src/components/ButtonText/styles.js
+++ RocketNotes/src/components/ButtonText/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.button`
   background: none;
-  color: ${({ theme, isActive }) =>
-    isActive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100}; // passando a prop. isActive e usando condicional.
+  color: ${({ theme, $isactive }) =>
+    $isactive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100}; // passando a prop. isActive e usando condicional.
 
   border: none;
   font-size: 16px;
