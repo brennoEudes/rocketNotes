e35875770f106ef741f3a4dcf523a91e4a9b5e9e
“Brenno <dev.brennoeudes@gmail.com>
2024-05-08 16:29:11 -0300

    SignIn page form

--- RocketNotes/src/pages/SignIn/index.jsx
+++ RocketNotes/src/pages/SignIn/index.jsx
@@ -1,9 +1,28 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
-export function SignIn(){
-    return(
-        <Container>
+import { FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-        </Container>
-    );
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>RocketNotes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+        <Input 
+        type="text"
+        placeholder="E-mail" 
+        icon={FiMail} />
+        <Input 
+        type="password" 
+        placeholder="Senha" 
+        icon={FiLock} />
+        <Button title="Entrar" />
+        <a href="#">Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
