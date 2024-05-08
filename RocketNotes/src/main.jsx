5660446f3e85ad380850c82f17248b7db6469aa5
“Brenno <dev.brennoeudes@gmail.com>
2024-05-08 15:57:20 -0300

    SignIn page structure

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyle from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
 
 // O ReactDom vai renderizar a div "root"!
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
