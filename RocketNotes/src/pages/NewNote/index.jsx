992472fb492a7374a9c20ed6138172e9c9a2b8fb
“Brenno <dev.brennoeudes@gmail.com>
2024-05-09 14:25:53 -0300

    Newnote form

--- RocketNotes/src/pages/NewNote/index.jsx
+++ RocketNotes/src/pages/NewNote/index.jsx
@@ -1,10 +1,25 @@
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Input } from "../../components/Input";
+// import { TextArea } from "../../components/TextArea";
+
+import { Container, Form } from "./styles";
 
 export function NewNote() {
   return (
     <Container>
       <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título"/>
+
+          {/* <TextArea placeholder="Observações" /> */}
+        </Form>
+      </main>
     </Container>
   );
 }
--- RocketNotes/src/pages/NewNote/styles.js
+++ RocketNotes/src/pages/NewNote/styles.js
@@ -6,8 +6,25 @@ export const Container = styled.div`
 
   display: grid;
   grid-template-rows: 105px auto;
-  grid-template-areas: 
-  "header"
-  "content"
-  ;
+  grid-template-areas:
+    "header"
+    "content";
+`;
+
+export const Form = styled.form`
+  max-width: 550px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 36px;
+
+    a {
+      font-size: 20px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
 `;
