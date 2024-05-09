9fc34e599f8a9f8b977fa290cb9a7e18ab0e8a3c
“Brenno <dev.brennoeudes@gmail.com>
2024-05-09 16:17:48 -0300

    appRoutes

---
+++ RocketNotes/src/routes/app.routes.jsx
@@ -0,0 +1,21 @@
+// rotas da aplicação c/ usuário logado:
+import { Routes, Route } from "react-router-dom";
+
+// importando as páginas acessadas após o login:
+import { Home } from "../pages/Home";
+import { NewNote } from "../pages/NewNote";
+import { Profile } from "../pages/Profile";
+import { Details } from "../pages/Details";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      {/* Envolve todas as rotas */}
+      <Route path="/" element={<Home />} />
+      {/* P/ cada rota, informamos o endereço e o q será renderizado! */}
+      <Route path="/new-note" element={<NewNote />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} /> {/* precisa do cód da nota, por isso add ":id" */}
+    </Routes>
+  );
+}
