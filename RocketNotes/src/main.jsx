84628d848b04b2fcf5530314bcf0129c923ddb10
“Brenno <dev.brennoeudes@gmail.com>
2024-05-09 10:55:04 -0300

    NewNote page first structure

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyle from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { Profile } from "./pages/Profile";
+import { NewNote } from "./pages/NewNote";
 
 // O ReactDom vai renderizar a div "root"!
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <NewNote />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ RocketNotes/src/pages/NewNote/index.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+import { Container } from "./styles";
+
+export function NewNote() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
