11f51f01595de0b3b1faae28c449b106b9024136
“Brenno <dev.brennoeudes@gmail.com>
2024-05-29 18:03:22 -0300

    routes redirect

--- RocketNotes/src/routes/index.jsx
+++ RocketNotes/src/routes/index.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuth(); // acessando o user
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {
+        // se existir user, direciona p/ AppRoutes. Se não, AuthRoutes:
+        user ? <AppRoutes /> : <AuthRoutes />
+      }
     </BrowserRouter>
   );
 }
