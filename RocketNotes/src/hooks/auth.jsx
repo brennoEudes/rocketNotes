14880f3f6fc51a8dd5e34389146d6803dcd15559
“Brenno <dev.brennoeudes@gmail.com>
2024-05-29 16:45:35 -0300

    signIn Auth function

--- RocketNotes/src/hooks/auth.jsx
+++ RocketNotes/src/hooks/auth.jsx
@@ -1,21 +1,48 @@
 // Hooks: Centralizam toda a lógica de utilização dos ctxts:
 
-import { createContext, useContext } from "react"; // famoso context API do react
+import { createContext, useContext, useState } from "react"; // createContext é o famoso context API do react
+
+import { api } from "../services/api"; // importe api p/ enviar dados p/ backend
 
 export const AuthContext = createContext({}); // define o valor padrão do contexto (ex: obj vazio)
 
 // lógica de ctxt no hook de auth:
 function AuthProvider({ children }) {
+  // Estados sempre no início da fcn:
+  const [data, setData] = useState({});
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      const { user, token } = response.data;
+
+      // LEITURA DE TRÁS PRA FRENTE!
+      // inserindo um token do tipo bearer, de auth, no cabeçalho, por padrão, de todas as REQs do user!
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({user, token}); // armazenando infos
+
+      console.log(user, token);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar!");
+      }
+    }
+  }
+
   // recebendo o children do AuthProvider lá do main.js, no caso, é o componente Routes.
   return (
-    <AuthContext.Provider
-      value={{ name: "Brenno", email: "brenno@teste.com" }}
-    > {/* O contexto provê um valor p/ todas as rotas */}
+    // compartilhando a fcn signIn e o estado "data" no provider (ctxt)
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {" "}
+      {/* O contexto provê um valor p/ todas as rotas */}
       {children}
     </AuthContext.Provider>
   );
 }
 
+// fcn c/ ctxt
 function useAuth() {
   const context = useContext(AuthContext);
   return context;
--- RocketNotes/src/pages/SignIn/index.jsx
+++ RocketNotes/src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"; // estado p/ armazenar informações inseridas nos inputs
 import { Container, Form, Background } from "./styles";
 import { Link } from "react-router-dom"; // p/ navegação
 
@@ -8,8 +9,15 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 export function SignIn() {
-  const data = useAuth(); // acessando o hook
-  console.log("Meu contexto:", data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth(); // acessando o hook
+
+  // CONCEITO: handle é um padrão p/ fcns q são disparadas em decorrência d euma interação do usuário.
+  function handleSignIn() {
+    signIn({ email, password }); // envia p/ fcn signIn lá no auth.jsx q precisa desses campos.
+  }
 
   return (
     <Container>
@@ -18,9 +26,19 @@ export function SignIn() {
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Faça seu login</h2>
-        <Input type="text" placeholder="E-mail" icon={FiMail} />
-        <Input type="password" placeholder="Senha" icon={FiLock} />
-        <Button title="Entrar" />
+        <Input
+          type="text"
+          placeholder="E-mail"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button title="Entrar" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
       <Background />
