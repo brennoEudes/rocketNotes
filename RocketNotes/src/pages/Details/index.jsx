b5b613e15f72fbcebeb7c5921cb5680ad6059541
“Brenno <dev.brennoeudes@gmail.com>
2024-06-05 19:46:46 -0300

    noteDetails, handleBack fcn

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,48 +1,69 @@
+import { useState, useEffect } from "react";
 import { Container, Links, Content } from "./style";
+import { useParams, useNavigate } from "react-router-dom"; // params permite a busca dos parâmetros passados na rota
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
+import { api } from "../../services/api";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/")
+
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
+
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis
-            neque, efficitur non odio sed, congue blandit odio. Sed finibus
-            risus id massa faucibus pharetra. Cras hendrerit tristique est ac
-            aliquam. Maecenas imperdiet enim neque, a tempor elit consequat sit
-            amet. Morbi in aliquam ipsum, ac feugiat enim. Praesent sit amet ex
-            eget sapien pretium semper ac vitae mi. Nam tortor orci, commodo eu
-            semper eu, ultricies eu sapien. Duis ultricies a ligula at feugiat.
-            In est orci, luctus vitae semper quis, congue eget ex.
-          </p>
-          <Section title="Links Úteis">
-            {/* O children não é passado como prop. comum, mas captura tudo q está dentro (abaixo): */}
-            <Links>
-              <li>
-                <a href="#">https://www.rockeseat.com.br/</a>
-              </li>
-              <li>
-                <a href="#">https://www.rockeseat.com.br/</a>
-              </li>
-            </Links>
-          </Section>
-          <Section title="Marcadores">
-            <Tag title="Express" />
-            <Tag title="NodeJS" />
-          </Section>
-          {/* <Button title="Login" loading /> loading é prop. boleana e ñ é necessario dizer true/false */}
-          <Button title="Voltar" />
-        </Content>
-      </main>
+      {data && ( // main só será mostrado se houver conteúdo!
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            {data.links && ( // a sessão só será renderizada se houver links!
+              <Section title="Links Úteis">
+                {/* O children não é passado como prop. comum, mas captura tudo q está dentro (abaixo): */}
+                <Links>
+                  data.links.map((link) => (
+                  <li key={String(link.id)}>
+                    <a href="{link.url}" target="_blank">{link.url}</a>
+                  </li>
+                  ))
+                </Links>
+              </Section>
+            )}
+
+            {data.tags && (
+              <Section title="Marcadores" >
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.title} />
+                ))}
+              </Section>
+            )}
+
+            {/* <Button title="Login" loading /> loading é prop. boleana e ñ é necessario dizer true/false */}
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
