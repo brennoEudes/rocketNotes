c18ce86c66637f61dd1cd4ecd49b57e4c54f3e7c
“Brenno <dev.brennoeudes@gmail.com>
2024-07-08 21:01:48 -0300

    console comment

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -32,7 +32,7 @@ export function Details() {
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
-      console.log(response.data); // Adicione esta linha para inspecionar os dados recebidos
+      //console.log(response.data); // Adicione esta linha para inspecionar os dados recebidos
       setData(response.data);
     }
 
